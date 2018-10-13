fb6ed1eac18ed9626ff310e2a7ac830205c6cba2
Zhixing Zhang <me@neoto.xin>
2018-10-12 22:34:11 -0400

    Init three.js

---
+++ src/views/Visualization.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="visualization-container"></div>
+</template>
+
+<script>
+import visualizationDrawer from '@/visualizations'
+export default {
+  name: 'Visualization',
+  mounted () {
+    visualizationDrawer(this.$el)
+  }
+}
+</script>
+
+<style scoped>
+  #visualization-container {
+    height: 100%;
+    width: 100%;
+  }
+</style>
