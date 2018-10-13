8802daa580b66fa259178e43ce7da3804c0d891b
James <zhaoziqi200@gmail.com>
2018-10-13 13:28:18 -0400

    Minor functionality
    Loading screen, Logout button and 404 screen

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-flex>
+    <v-progress-circular
+      :size="100"
+      :width="10"
+      color="blue"
+      indeterminate
+    ></v-progress-circular>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  name: 'LoadingScreen'
+}
+</script>
+
+<style scoped>
+
+</style>
