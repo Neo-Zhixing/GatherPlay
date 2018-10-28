94fc30168230c9e3d077339902288be7f9379412
Zhixing Zhang <me@neoto.xin>
2018-10-27 22:37:01 -0400

    Stepping View & Google Maps

--- src/views/NewEvent.vue
+++ src/views/NewEvent.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
   v-card
-    v-tabs(centered v-model="form.authentication")
-      v-tab(v-for="(item, index) in authOptions" :key="index")
-        span(v-text="item.title")
-        v-icon(v-text="item.icon")
-    v-card-title(primary-title): h3.headline.mb-0 Create a New Event
-    v-card-text: v-form(v-model="formValid")
-      v-layout(wrap)
+    v-stepper(v-model="step" non-linear vertical)
+      v-stepper-step(
+        :complete="step > 1"
+        editable
+        step="1"
+        :rules="[() => formValid]"
+      )
+        span Basics
+      v-stepper-content(:step="1"): v-form(v-model="formValid"): v-layout(wrap)
         v-flex(xs12 sm6): v-text-field(
           label="ID"
           :rules="rules"
@@ -21,17 +23,52 @@
           v-model="form.name"
           hint="The Human Readable Name"
         )
-      v-text-field(
-        v-show="form.authentication === 1"
-        label="Password"
-        v-model="form.password"
-      )
-      v-checkbox(
-        v-show="form.authentication !== 3"
-        label="Needs Approval"
-        v-model="form.approval_needed"
-      )
-      p(v-text="hint").caption.mb-0.mt-3
+        v-textarea(
+          label="Description"
+          v-model="form.description"
+        )
+      v-stepper-step(:complete="step > 2" editable :step="2")
+        span Authentication
+        small Who can join the event
+      v-stepper-content(:step="2")
+        v-select(
+          :items="authOptions"
+          v-model="form.authentication"
+        )
+        v-text-field(
+          v-show="form.authentication === 1"
+          label="Password"
+          v-model="form.password"
+        )
+        v-checkbox(
+          v-show="form.authentication !== 3"
+          label="Needs Approval"
+          v-model="form.approval_needed"
+        )
+        p(v-text="hint").caption
+      v-stepper-step(:complete="step > 3" editable :step="3")
+        span Location
+        small Help your guests find the event
+      v-stepper-content(:step="3")
+        v-switch(label="Enable" v-model="form.enable_location")
+        v-switch(label="Use my Realtime Location" :disabled="!form.enable_location" v-model="form.realtime_location")
+        googlemaps-map(
+          v-if="form.enable_location && !form.realtime_location"
+          style="height: 10rem;"
+          :center="currentLocation"
+          :zoom="15"
+          @click="mapClicked"
+        )
+          googlemaps-user-position(@update:position="updateUserPosition")
+          googlemaps-marker(
+            :label="{\
+              color: 'white',\
+              fontFamily: 'Material Icons',\
+              fontSize: '20px',\
+              text: 'star_rate',\
+            }"
+            :position="form.location || {lat: 0, lng: 0}"
+          )
     v-card-actions
       v-spacer
       v-btn(flat @click="createEvent" :disabled="!formValid" :loading="loading") Create
@@ -41,27 +78,36 @@
 <script>
 import { mapState, mapActions } from 'vuex'
 import config from '@/../config.json'
-import { db } from '@/plugins/firebase'
+import firebase, { db } from '@/plugins/firebase'
 const authOptions = [
-  { title: 'Open', icon: 'lock_open', des: 'Anyone can join the event' },
-  { title: 'Password', icon: 'account_box', des: 'Only guests with password could join the event' },
-  { title: 'Invitation', icon: 'link', des: 'Only guests refereed by old members could join the event' },
-  { title: 'Closed', icon: 'lock', des: 'Only you can add new members into the event' },
+  { text: 'Open', value: 0, des: 'Anyone can join the event' },
+  { text: 'Password', value: 1, des: 'Only guests with password could join the event' },
+  { text: 'Invitation', value: 2, des: 'Only guests refereed by old members could join the event' },
+  { text: 'Closed', value: 3, des: 'Only you can add new members into the event' },
 ]
 export default {
   name: 'newevent',
   data () {
     return {
+      step: 1,
       authOptions: authOptions,
       formValid: false,
       idTaken: false,
       eventID: '',
       loading: false,
+      currentLocation: {
+        lat: 0,
+        lng: 0,
+      },
       form: {
         authentication: 0,
         name: '',
         password: null,
         approval_needed: false,
+        location: null,
+        enable_location: true,
+        realtime_location: true,
+        description: '',
       }
     }
   },
@@ -72,16 +118,18 @@ export default {
     createEvent () {
       this.loading = true
       const docRef = db.collection('events').doc(this.eventID)
-      if (this.form.password === '') {
-        this.form.password = null
-      }
       docRef.get()
         .then(doc => {
           if (doc.exists) {
             this.idTaken = true
+            this.step = 1
             return
           }
-          return docRef.set(this.form)
+          const form = Object.assign({}, this.form)
+          if (form.password === '') form.password = null
+          form.location = form.enable_location && !form.realtime_location ?
+            new firebase.firestore.GeoPoint(form.location.lat, form.location.lng) : null
+          return docRef.set(form)
             .then(() => {
               this.$emit('created', this.eventID)
             })
@@ -91,6 +139,22 @@ export default {
           this.loading = false
         })
     },
+    mapClicked (event) {
+      this.form.location = {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      }
+    },
+    updateUserPosition (event) {
+      this.currentLocation.lat = event.lat
+      this.currentLocation.lng = event.lng
+      if (!this.form.location) {
+        this.form.location = {
+          lat: event.lat,
+          lng: event.lng,
+        }
+      }
+    }
   },
   computed: {
     ...mapState({
